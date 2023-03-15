d5213d658b8bfa75510580de1aa52b5af9230533
Tretiak Serhii <tretyak8936@gmail.com>
2023-03-15 22:36:59 +0200

    created product card

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Header } from './Components/Header';
+import { ProductCard } from './Components/ProductCard';
 
 export function App() {
   return (
     <div className="App">
       <Header />
+      <ProductCard />
     </div>
   );
 }
