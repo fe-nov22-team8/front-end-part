0e170c16ae4096d15e2b00096e737c21881bdc08
Brandon Sunrise <stepansky93@gmail.com>
2023-03-16 19:03:39 +0200

    add cart page

---
+++ src/Components/Cart/Cart.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Cart.scss';
+
+export const Cart: React.FC = () => (
+  <div className="container">
+    <div className="cart grid">
+      <Link
+        to="/"
+        className="nav__link
+            cart__link--back
+            grid__item--desktop-1-4
+            grid__item--tablet-1-4
+            grid__item--mobile-1-4"
+      >
+        <span className="cart__arrow" />
+        Back
+      </Link>
+
+      <h1
+        className="cart__h1
+            grid__item--desktop-1-4
+            grid__item--tablet-1-4
+            grid__item--mobile-1-4"
+      >
+        Cart
+      </h1>
+    </div>
+  </div>
+);
